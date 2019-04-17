d1d3645f354bdee2fb6c5f5c441f437066f09575
Alois Beard <alois.beard@gmail.com>
2019-04-18 00:21:03 +0200

    text appear when video starts

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const overlay = document.querySelector('.overlay');
+const video = document.querySelector('.video');
 
+video.addEventListener('play', (event) => {
+  overlay.style.visibility = "visible";
+});
+
+window.addEventListener('beforeunload', (event) => {
+  overlay.style.visibility = "hidden";
+});
